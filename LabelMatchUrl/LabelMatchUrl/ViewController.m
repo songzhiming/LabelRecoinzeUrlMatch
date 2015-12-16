ffe2307bebda78525363e3d26135256dfcdc545b
宋志明 <15117951262@163.com>
2015-12-16 13:34:11 +0800

    初始化项目

---
+++ LabelMatchUrl/LabelMatchUrl/ViewController.m
@@ -0,0 +1,245 @@
+//
+//  ViewController.m
+//  LabelMatchUrl
+//
+//  Created by 宋志明 on 15/12/16.
+//  Copyright © 2015年 宋志明. All rights reserved.
+//
+
+#define LABEL_FONT_SIZE 14      // 文字大小
+#define LABEL_LINESPACE 5       // 行间距
+
+#import "ViewController.h"
+#import <CoreText/CoreText.h>
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (strong,nonatomic)NSDataDetector *detector;
+@property (strong,nonatomic)NSArray *urlMatches;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    _detector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink error:nil];
+    [self setModel:@"www.baidu.com你好http://www.jianshu.com/"];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tap:)];
+    [self.label addGestureRecognizer:tap];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)tap:(UITapGestureRecognizer *)sender
+{
+    CGPoint point = [sender locationInView:_label];
+    CFIndex charIndex = [self characterIndexAtPoint:point];
+    
+    [self highlightLinksWithIndex:NSNotFound];
+    
+    for (NSTextCheckingResult *match in _urlMatches) {
+        if ([match resultType] == NSTextCheckingTypeLink) {
+            NSRange matchRange = [match range];
+            if ([self isIndex:charIndex inRange:matchRange]) {
+                NSLog(@"====%@",match.URL);
+                //                [self routerEventWithName:kRouterEventTextURLTapEventName userInfo:@{KMESSAGEKEY:self.model, @"url":match.URL}];
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+- (void)setModel:(NSString *)string
+{
+    
+    _urlMatches = [_detector matchesInString:string options:0 range:NSMakeRange(0, string.length)];
+    NSMutableAttributedString * attributedString = [[NSMutableAttributedString alloc]
+                                                    initWithString:string];
+    NSMutableParagraphStyle * paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    [paragraphStyle setLineSpacing:LABEL_LINESPACE];
+    [attributedString addAttribute:NSParagraphStyleAttributeName
+                             value:paragraphStyle
+                             range:NSMakeRange(0, [string length])];
+    [_label setAttributedText:attributedString];
+    [self highlightLinksWithIndex:NSNotFound];
+}
+
+- (BOOL)isIndex:(CFIndex)index inRange:(NSRange)range
+{
+    return index > range.location && index < range.location+range.length;
+}
+
+- (void)highlightLinksWithIndex:(CFIndex)index {
+    
+    NSMutableAttributedString* attributedString = [_label.attributedText mutableCopy];
+    
+    for (NSTextCheckingResult *match in _urlMatches) {
+        
+        if ([match resultType] == NSTextCheckingTypeLink) {
+            
+            NSRange matchRange = [match range];
+            
+            if ([self isIndex:index inRange:matchRange]) {
+                [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor grayColor] range:matchRange];
+            }
+            else {
+                [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:matchRange];
+            }
+            
+            [attributedString addAttribute:NSUnderlineStyleAttributeName value:[NSNumber numberWithInteger:NSUnderlineStyleSingle] range:matchRange];
+        }
+    }
+    
+    _label.attributedText = attributedString;
+}
+
+- (CFIndex)characterIndexAtPoint:(CGPoint)point
+{
+    NSMutableAttributedString* optimizedAttributedText = [_label.attributedText mutableCopy];
+    
+    // use label's font and lineBreakMode properties in case the attributedText does not contain such attributes
+    [_label.attributedText enumerateAttributesInRange:NSMakeRange(0, [_label.attributedText length]) options:0 usingBlock:^(NSDictionary *attrs, NSRange range, BOOL *stop) {
+        
+        if (!attrs[(NSString*)kCTFontAttributeName])
+        {
+            [optimizedAttributedText addAttribute:(NSString*)kCTFontAttributeName value:_label.font range:NSMakeRange(0, [_label.attributedText length])];
+        }
+        
+        if (!attrs[(NSString*)kCTParagraphStyleAttributeName])
+        {
+            NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+            [paragraphStyle setLineBreakMode:_label.lineBreakMode];
+            
+            [optimizedAttributedText addAttribute:(NSString*)kCTParagraphStyleAttributeName value:paragraphStyle range:range];
+        }
+    }];
+    
+    // modify kCTLineBreakByTruncatingTail lineBreakMode to kCTLineBreakByWordWrapping
+    [optimizedAttributedText enumerateAttribute:(NSString*)kCTParagraphStyleAttributeName inRange:NSMakeRange(0, [optimizedAttributedText length]) options:0 usingBlock:^(id value, NSRange range, BOOL *stop)
+     {
+         NSMutableParagraphStyle* paragraphStyle = [value mutableCopy];
+         
+         if ([paragraphStyle lineBreakMode] == NSLineBreakByTruncatingTail) {
+             [paragraphStyle setLineBreakMode:NSLineBreakByWordWrapping];
+         }
+         
+         [optimizedAttributedText removeAttribute:(NSString*)kCTParagraphStyleAttributeName range:range];
+         [optimizedAttributedText addAttribute:(NSString*)kCTParagraphStyleAttributeName value:paragraphStyle range:range];
+     }];
+    
+    if (!CGRectContainsPoint(self.label.bounds, point)) {
+        return NSNotFound;
+    }
+    
+    CGRect textRect = _label.bounds;
+    
+//    if (!CGRectContainsPoint(textRect, point)) {
+//        return NSNotFound;
+//    }
+    
+    // Offset tap coordinates by textRect origin to make them relative to the origin of frame
+    point = CGPointMake(point.x - textRect.origin.x, point.y - textRect.origin.y);
+    // Convert tap coordinates (start at top left) to CT coordinates (start at bottom left)
+    point = CGPointMake(point.x, textRect.size.height - point.y);
+    
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((__bridge CFAttributedStringRef)optimizedAttributedText);
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, textRect);
+    
+    CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, [_label.attributedText length]), path, NULL);
+    
+    if (frame == NULL) {
+        CFRelease(path);
+        return NSNotFound;
+    }
+    
+    CFArrayRef lines = CTFrameGetLines(frame);
+    
+    NSInteger numberOfLines = _label.numberOfLines > 0 ? MIN(_label.numberOfLines, CFArrayGetCount(lines)) : CFArrayGetCount(lines);
+    
+    //NSLog(@"num lines: %d", numberOfLines);
+    
+    if (numberOfLines == 0) {
+        CFRelease(frame);
+        CFRelease(path);
+        return NSNotFound;
+    }
+    
+    NSUInteger idx = NSNotFound;
+    
+    CGPoint lineOrigins[numberOfLines];
+    CTFrameGetLineOrigins(frame, CFRangeMake(0, numberOfLines), lineOrigins);
+    
+    for (CFIndex lineIndex = 0; lineIndex < numberOfLines; lineIndex++) {
+        
+        CGPoint lineOrigin = lineOrigins[lineIndex];
+        CTLineRef line = CFArrayGetValueAtIndex(lines, lineIndex);
+        
+        // Get bounding information of line
+        CGFloat ascent, descent, leading, width;
+        width = CTLineGetTypographicBounds(line, &ascent, &descent, &leading);
+        CGFloat yMin = floor(lineOrigin.y - descent);
+        CGFloat yMax = ceil(lineOrigin.y + ascent);
+        
+        // Check if we've already passed the line
+        if (point.y > yMax) {
+            break;
+        }
+        
+        // Check if the point is within this line vertically
+        if (point.y >= yMin) {
+            
+            // Check if the point is within this line horizontally
+            if (point.x >= lineOrigin.x && point.x <= lineOrigin.x + width) {
+                
+                // Convert CT coordinates to line-relative coordinates
+                CGPoint relativePoint = CGPointMake(point.x - lineOrigin.x, point.y - lineOrigin.y);
+                idx = CTLineGetStringIndexForPosition(line, relativePoint);
+                
+                break;
+            }
+        }
+    }
+    
+    CFRelease(frame);
+    CFRelease(path);
+    
+    return idx;
+}
+
+//#pragma mark - public
+//
+//-(void)bubbleViewPressed:(id)sender
+//{
+//    UITapGestureRecognizer *tap = (UITapGestureRecognizer *)sender;
+//    CGPoint point = [tap locationInView:_label];
+//    CFIndex charIndex = [self characterIndexAtPoint:point];
+//    
+//    [self highlightLinksWithIndex:NSNotFound];
+//    
+//    for (NSTextCheckingResult *match in _urlMatches) {
+//        
+//        if ([match resultType] == NSTextCheckingTypeLink) {
+//            
+//            NSRange matchRange = [match range];
+//            
+//            if ([self isIndex:charIndex inRange:matchRange]) {
+//                
+////                [self routerEventWithName:kRouterEventTextURLTapEventName userInfo:@{KMESSAGEKEY:self.model, @"url":match.URL}];
+//                break;
+//            }
+//        }
+//    }
+//}
+
+@end
